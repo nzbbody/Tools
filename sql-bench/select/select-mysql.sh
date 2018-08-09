905e30853996fa03c8527846220466a0eef44378
niuzibin <niuzibin@flksec.com>
2018-08-09 00:14:02 -0700

    增加select的测试代码

--- sql-bench/select/select-mysql.sh
+++ sql-bench/select/select-mysql.sh
@@ -7,6 +7,4 @@ database=sql_bench
 
 sed "20c port            = ${port}" -i my.cnf
 
-mysqladmin create ${database} -h127.0.0.1 -P${port} -uroot -pletmein
 ./$shFile --server=mysql --host=127.0.0.1 --user=root --password=letmein --database=${database} --connect-options=mysql_read_default_file=./my.cnf
-
