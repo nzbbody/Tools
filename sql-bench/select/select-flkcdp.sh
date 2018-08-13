eb190522da7bc80b01d3f1a994461085a7dc9060
niuzibin <niuzibin@flksec.com>
2018-08-13 04:06:36 -0700

    增加host的配置

--- sql-bench/select/select-flkcdp.sh
+++ sql-bench/select/select-flkcdp.sh
@@ -3,9 +3,10 @@ cd $shPath
 set -e
 
 shFile=test-select.sh
+host=127.0.0.1
 port=3307
 database=sql_bench
 
 sed "20c port            = ${port}" -i my.cnf
 
-./$shFile --server=mysql --host=127.0.0.1 --user=root --password=letmein --database=${database} --connect-options=mysql_read_default_file=./my.cnf
+./$shFile --server=mysql --host=$host --user=root --password=letmein --database=${database} --connect-options=mysql_read_default_file=./my.cnf
--- sql-bench/select/select-mysql.sh
+++ sql-bench/select/select-mysql.sh
@@ -2,9 +2,10 @@ shPath=$(cd $(dirname $0);pwd)
 cd $shPath
 
 shFile=test-select.sh
+host=127.0.0.1
 port=3306
 database=sql_bench
 
 sed "20c port            = ${port}" -i my.cnf
 
-./$shFile --server=mysql --host=127.0.0.1 --user=root --password=letmein --database=${database} --connect-options=mysql_read_default_file=./my.cnf
+./$shFile --server=mysql --host=$host --user=root --password=letmein --database=${database} --connect-options=mysql_read_default_file=./my.cnf
